a42f04bcf8b9afead26ef4be773a2747d8ef25d5
Matheus de Sá <matheus.de.sa@outlook.com.br>
2025-09-23 21:53:53 -0300

    Update S3 bucket names and streamline Terraform apply commands in deployment workflows

--- middleware-service/terraform/lambda.tf
+++ middleware-service/terraform/lambda.tf
@@ -1,10 +1,10 @@
-resource "aws_s3_bucket" "lambda-middleware-service-bucket" {
-  bucket = "lambda-middleware-service-bucket"
+resource "aws_s3_bucket" "soat-credential-lambdas" {
+  bucket = "soat-credential-lambdas"
 }
 
 resource "aws_s3_object" "lambda_middleware-service_zip" {
-  bucket = aws_s3_bucket.lambda-middleware-service-bucket.id
-  key    = "auth-service.zip"
+  bucket = aws_s3_bucket.soat-credential-lambdas.id
+  key    = "middleware-service.zip"
   source = "${path.module}/../../middleware-service.zip"
 }
 
@@ -16,7 +16,7 @@ resource "aws_lambda_function" "middleware" {
   memory_size   = 512
   timeout       = 15
 
-  s3_bucket        = aws_s3_bucket.lambda-middleware-service-bucket.id
+  s3_bucket        = aws_s3_bucket.soat-credential-lambdas.id
   s3_key           = aws_s3_object.lambda_middleware-service_zip.key
   source_code_hash = aws_s3_object.lambda_middleware-service_zip.etag
 
